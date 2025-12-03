600314115b17a90bba8c950525e74d8a9ed0704f
GoguX76 <se.godoyr@duocuc.cl>
2025-12-03 12:01:09 -0300

    Modificación LoginRequest
    Modificación en el código de LoginRequest para usar Lombok y no definir los Getter y Setters manualmente como antes estaba.

--- src/main/java/com/example/midnight_phonk_api/Midnight_Phonk_Api/dto/LoginRequest.java
+++ src/main/java/com/example/midnight_phonk_api/Midnight_Phonk_Api/dto/LoginRequest.java
@@ -1,8 +1,16 @@
 package com.example.midnight_phonk_api.Midnight_Phonk_Api.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class LoginRequest {
 
     @NotEmpty
@@ -12,20 +20,4 @@ public class LoginRequest {
     @NotEmpty
     private String password;
 
-    // Getters y Setters
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
